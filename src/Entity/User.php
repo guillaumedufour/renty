25400b37050ef6eb32883def53078763af3d6db6
Guillaume D <dufour.g@live.fr>
2018-06-29 13:51:26 +0200

    gestion des utilisateurs est basique mais fonctionnelle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,16 +2,20 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="user")
+ * @UniqueEntity(fields="email", message="Email déjà pris")
+ * @UniqueEntity(fields="username", message="Username déjà pris")
  */
-class User implements UserInterface, \Serializable {
-
+class User implements UserInterface, \Serializable
+{
     /**
      * @var int
      *
@@ -25,6 +29,7 @@ class User implements UserInterface, \Serializable {
      * @var string
      *
      * @ORM\Column(type="string")
+     * @Assert\NotBlank()
      */
     private $fullName;
 
@@ -32,6 +37,7 @@ class User implements UserInterface, \Serializable {
      * @var string
      *
      * @ORM\Column(type="string", unique=true)
+     * @Assert\NotBlank()
      */
     private $username;
 
@@ -39,13 +45,15 @@ class User implements UserInterface, \Serializable {
      * @var string
      *
      * @ORM\Column(type="string", unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
 
     /**
      * @var string
      *
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", length=64)
      */
     private $password;
 
@@ -56,46 +64,57 @@ class User implements UserInterface, \Serializable {
      */
     private $roles = [];
 
-    public function getId(): int {
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function setFullName(string $fullName): void {
+    public function setFullName(string $fullName): void
+    {
         $this->fullName = $fullName;
     }
 
-    public function getFullName(): string {
+    // le ? signifie que cela peur aussi retourner null
+    public function getFullName(): ?string
+    {
         return $this->fullName;
     }
 
-    public function getUsername(): string {
+    public function getUsername(): ?string
+    {
         return $this->username;
     }
 
-    public function setUsername(string $username): void {
+    public function setUsername(string $username): void
+    {
         $this->username = $username;
     }
 
-    public function getEmail(): string {
+    public function getEmail(): ?string
+    {
         return $this->email;
     }
 
-    public function setEmail(string $email): void {
+    public function setEmail(string $email): void
+    {
         $this->email = $email;
     }
 
-    public function getPassword(): string {
+    public function getPassword(): ?string
+    {
         return $this->password;
     }
 
-    public function setPassword(string $password): void {
+    public function setPassword(string $password): void
+    {
         $this->password = $password;
     }
 
     /**
      * Retourne les rôles de l'user
      */
-    public function getRoles(): array {
+    public function getRoles(): array
+    {
         $roles = $this->roles;
 
         // Afin d'être sûr qu'un user a toujours au moins 1 rôle
@@ -106,7 +125,8 @@ class User implements UserInterface, \Serializable {
         return array_unique($roles);
     }
 
-    public function setRoles(array $roles): void {
+    public function setRoles(array $roles): void
+    {
         $this->roles = $roles;
     }
 
@@ -115,7 +135,8 @@ class User implements UserInterface, \Serializable {
      *
      * {@inheritdoc}
      */
-    public function getSalt(): ?string {
+    public function getSalt(): ?string
+    {
         // See "Do you need to use a Salt?" at https://symfony.com/doc/current/cookbook/security/entity_provider.html
         // we're using bcrypt in security.yml to encode the password, so
         // the salt value is built-in and you don't have to generate one
@@ -128,7 +149,8 @@ class User implements UserInterface, \Serializable {
      *
      * {@inheritdoc}
      */
-    public function eraseCredentials(): void {
+    public function eraseCredentials(): void
+    {
         // Nous n'avons pas besoin de cette methode car nous n'utilions pas de plainPassword
         // Mais elle est obligatoire car comprise dans l'interface UserInterface
         // $this->plainPassword = null;
@@ -137,15 +159,16 @@ class User implements UserInterface, \Serializable {
     /**
      * {@inheritdoc}
      */
-    public function serialize(): string {
+    public function serialize(): string
+    {
         return serialize([$this->id, $this->username, $this->password]);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function unserialize($serialized): void {
+    public function unserialize($serialized): void
+    {
         [$this->id, $this->username, $this->password] = unserialize($serialized, ['allowed_classes' => false]);
     }
-
 }
